75c25456fbd5f38a7749c1f08e6a3fe29d8ae3c3
a-jwc <chauajw@gmail.com>
2022-02-13 09:43:13 -0800

    stlying

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
 	return (
-		<div className="text-center text-lg h-36 justify-self-center col-start-2 col-end-3 z-10">
+		<div className="text-center text-lg h-36 justify-self-center col-start-2 col-end-3 z-10 mt-10">
 			<p>Happy Valentines Day Dani 💝</p>
 		</div>
 	);
