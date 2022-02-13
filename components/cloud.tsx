d2c06758caba9139415aadcd4027687dc783a911
a-jwc <chauajw@gmail.com>
2022-02-13 09:42:07 -0800

    useWindowSize in ts

--- components/cloud.tsx
+++ components/cloud.tsx
@@ -1,4 +1,4 @@
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, useSprings, useTransition } from "react-spring";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -7,36 +7,33 @@ interface Prop {
 	xPosition: String;
 }
 
-function useWindowSize() {
+interface Size {
+	width: number;
+	height: number;
+}
+
+function useWindowSize(): Size {
 	// Initialize state with undefined width/height so server and client renders match
 	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-	const [windowSize, setWindowSize] = useState({
+	const [windowSize, setWindowSize] = useState<Size>({
 		width: 0,
 		height: 0,
 	});
-
-	function handleResize() {
-		// Set window width/height to state
-		setWindowSize({
-			width: window.innerWidth,
-			height: window.innerHeight,
-		});
-	}
-
 	useEffect(() => {
-		// only execute all the code below in client side
-		if (typeof window !== "undefined") {
-			// Handler to call on window resize
-
-			// Add event listener
-			window.addEventListener("resize", handleResize);
-
-			// Call handler right away so state gets updated with initial window size
-			handleResize();
-
-			// Remove event listener on cleanup
-			return () => window.removeEventListener("resize", handleResize);
+		// Handler to call on window resize
+		function handleResize() {
+			// Set window width/height to state
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
 		}
+		// Add event listener
+		window.addEventListener("resize", handleResize);
+		// Call handler right away so state gets updated with initial window size
+		handleResize();
+		// Remove event listener on cleanup
+		return () => window.removeEventListener("resize", handleResize);
 	}, []); // Empty array ensures that effect is only run on mount
 	return windowSize;
 }
@@ -68,7 +65,7 @@ export const Cloud = ({ yPosition, xPosition }: Prop) => {
 	return (
 		<>
 			<animated.div
-				className={`absolute ${xPosition} ${yPosition}`}
+				className={`cloud absolute ${xPosition} ${yPosition}`}
 				style={props}
 			>
 				<Image
