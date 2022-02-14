9b7877ce52a1e5bb27c405548d02ef2f27148087
a-jwc <chauajw@gmail.com>
2022-02-14 01:48:01 -0800

    increase size

--- components/dance-gif.tsx
+++ components/dance-gif.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const DanceGif = () => {
 	return (
 		<>
-			<div className="header-container h-52 w-52 relative">
+			<div className="header-container h-56 w-56 relative">
 				<Image
 					src="/images/rilakkuma-dancing.gif"
 					layout="fill"
