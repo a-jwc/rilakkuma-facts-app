fb1008b7fcb912a9e055170a25b8050dcb46f27e
a-jwc <chauajw@gmail.com>
2022-02-14 13:39:55 -0800

    add links to images

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,24 +5,31 @@ export const Footer = () => {
 	return (
 		<>
 			<div className="header-container mx-auto relative pb-16">
-				<Image
-					src="/images/rila-yellow-2-transparent.png"
-          layout="responsive"
-          width={320}
-          height={320}
-					alt="icecream"
-					objectFit="cover"
-					className={`header-image`}
-				/>
+				<a
+					href="http://www.san-x.jp/characters/rilakkuma.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Image
+						src="/images/rila-yellow-2-transparent.png"
+						layout="responsive"
+						width={320}
+						height={320}
+						alt="icecream"
+						objectFit="cover"
+						className={`header-image`}
+					/>
+					1
+				</a>
 			</div>
 			<div className="header-container mx-auto relative pb-16">
 				<Link href="/happy-valentines-day">
 					<a>
 						<Image
 							src="/images/rila-icecream-transparent.png"
-              layout="responsive"
-              width={320}
-              height={320}
+							layout="responsive"
+							width={320}
+							height={320}
 							alt="icecream"
 							objectFit="cover"
 							className={`header-image`}
@@ -31,15 +38,21 @@ export const Footer = () => {
 				</Link>
 			</div>
 			<div className="header-container mx-auto relative pb-16">
-				<Image
-					src="/images/rila-yellow-transparent.png"
-					layout="responsive"
-          width={448}
-          height={384}
-					alt="icecream"
-					objectFit="cover"
-					className={`header-image`}
-				/>
+				<a
+					href="https://www.youtube.com/watch?v=228KoorFs-Y&list=PL2nNLUYH9TlSssFkWQz_rK2f-S6Rblk2N"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Image
+						src="/images/rila-yellow-transparent.png"
+						layout="responsive"
+						width={448}
+						height={384}
+						alt="icecream"
+						objectFit="cover"
+						className={`header-image`}
+					/>
+				</a>
 			</div>
 		</>
 	);
